67b6e241a83af658704e87e8871adc7137e2ea91
Mike Hornan [ABLE] <112007661+Hornan7@users.noreply.github.com>
2023-12-07 02:28:20 -0500

    Update drep-vote.sh

--- SanchoNet-Scripts/drep-vote.sh
+++ SanchoNet-Scripts/drep-vote.sh
@@ -2,16 +2,16 @@
 
 echo "What is the governance action ID?"
 read GOVID
-sleep 1
+sleep 0.5
 echo "What is its highest index number?"
 read INDEXNO
-sleep 1
+sleep 0.5
 echo "What is your Vote? yes,no,abstain?"
 read VOTE
 echo "------------------------------------------"
 echo "              Creating Vote"
 echo "------------------------------------------"
-sleep 1
+sleep 0.5
 #create the action file directory   
 mkdir action-votes 2>/dev/null
 
@@ -23,9 +23,8 @@ while true; do
                 --governance-action-tx-id "${GOVID}" \
                 --governance-action-index "${INDEXNO}" \
                 --drep-verification-key-file drep.vkey \
-
                 --out-file action-votes/action${INDEXNO}.vote
-                sleep 1
+                sleep 0.5
             echo " --vote-file action-votes/action${INDEXNO}.vote" >> action-votes/txvar.txt
             echo "         Preparing vote number ${INDEXNO}"
             INDEXNO=$((INDEXNO-1))
@@ -36,7 +35,7 @@ while true; do
                 --governance-action-index "${INDEXNO}" \
                 --drep-verification-key-file drep.vkey \
                 --out-file action-votes/action${INDEXNO}.vote
-                sleep 1
+                sleep 0.5
                 echo "         Preparing vote number ${INDEXNO}"
                 echo " --vote-file action-votes/action${INDEXNO}.vote" >> action-votes/txvar.txt
                 break #breaking the loop
@@ -46,37 +45,38 @@ done
 echo "------------------------------------------"
 echo "           Building Transaction"
 echo "------------------------------------------"
-sleep 1
+sleep 0.5
 
-#build the Transaction
-
-  cardano-cli conway transaction build \
-  --testnet-magic 4 \
-  --tx-in "$(cardano-cli query utxo --address $(cat payment.addr) --testnet-magic 4 --out-file /dev/stdout | jq -r 'keys[0]')" \
-  --change-address $(cat payment.addr) \
-  $(cat action-votes/txvar.txt) \
-  --witness-override 2 \
-  --out-file vote-tx.raw
-  rm action-votes/txvar.txt
+        #build the Transaction
+        cardano-cli conway transaction build \
+        --testnet-magic 4 \
+        --tx-in "$(cardano-cli query utxo --address $(cat payment.addr) --testnet-magic 4 --out-file /dev/stdout | jq -r 'keys[0]')" \
+        --change-address $(cat payment.addr) \
+        $(cat action-votes/txvar.txt) \
+        --witness-override 2 \
+        --out-file vote-tx.raw
+        
+# Remove the action index options file        
+rm action-votes/txvar.txt
   
 echo "           Signing Transaction"
 echo "------------------------------------------"
-sleep 1
+sleep 0.5
 
-#Sign the transaction
-
-  cardano-cli transaction sign --tx-body-file vote-tx.raw \
-  --signing-key-file drep.skey \
-  --signing-key-file payment.skey \
-  --testnet-magic 4 \
-  --out-file vote-tx.signed
+        #Sign the transaction
+        cardano-cli transaction sign --tx-body-file vote-tx.raw \
+        --signing-key-file drep.skey \
+        --signing-key-file payment.skey \
+        --testnet-magic 4 \
+        --out-file vote-tx.signed
 
 echo "      Submiting Transaction On-Chain"
 echo "------------------------------------------"
-sleep 1
+sleep 0.5
 
-#Submit the Transaction
-
-cardano-cli transaction submit --testnet-magic 4 --tx-file vote-tx.signed
+        #Submit the Transaction
+        cardano-cli transaction submit \
+        --testnet-magic 4 \
+        --tx-file vote-tx.signed
 
 echo "Vote complete on ${GOVID}"
